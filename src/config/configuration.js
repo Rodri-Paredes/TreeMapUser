4741926c9e866654571644c00790035eda54e180
rodrigorparedes <rodrigorparedes@gmail.com>
2024-10-19 18:59:42 -0400

    modified firebase key and refactor design

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -2,8 +2,8 @@ const configuration = {
     map: {
         zoom: 18,
         center: {
-            lat: -17.331015,
-            lng: -66.225959
+            lat: -17.393838,
+            lng: -66.157061
         },
         googleMapsApiKey: 'AIzaSyCYUX86b8TJ1yDLVcObptfb3XF5L19BYzg'
     }
--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -7,14 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyARizvajIEasBxw--QOK5BGWVTzdmBro3U",
-  authDomain: "tree-map-ae44c.firebaseapp.com",
-  databaseURL: "https://tree-map-ae44c-default-rtdb.firebaseio.com",
-  projectId: "tree-map-ae44c",
-  storageBucket: "tree-map-ae44c.appspot.com",
-  messagingSenderId: "481276251943",
-  appId: "1:481276251943:web:32e678670f70f9c2e3a302",
-  measurementId: "G-XY8WVM315R"
+  apiKey: "AIzaSyBJDU_NHHumcYDpsfSSAZCWhBUb-tCl1jc",
+  authDomain: "tree-map-alcaldia.firebaseapp.com",
+  databaseURL: "https://tree-map-alcaldia-default-rtdb.firebaseio.com",
+  projectId: "tree-map-alcaldia",
+  storageBucket: "tree-map-alcaldia.appspot.com",
+  messagingSenderId: "363928218840",
+  appId: "1:363928218840:web:ebba88f21405ced4323bad"
 };
 
 // Initialize Firebase
