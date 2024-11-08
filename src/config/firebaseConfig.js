314bf82b21097af4c7f4b5cba7cf1b29b6ac4759
rodrigorparedes <rodrigorparedes@gmail.com>
2024-11-07 22:41:51 -0400

    modified config firebase

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,23 +1,18 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyARizvajIEasBxw--QOK5BGWVTzdmBro3U",
-  authDomain: "tree-map-ae44c.firebaseapp.com",
-  databaseURL: "https://tree-map-ae44c-default-rtdb.firebaseio.com",
-  projectId: "tree-map-ae44c",
-  storageBucket: "tree-map-ae44c.appspot.com",
-  messagingSenderId: "481276251943",
-  appId: "1:481276251943:web:32e678670f70f9c2e3a302",
-  measurementId: "G-XY8WVM315R"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const config = initializeApp(firebaseConfig);
 
-export default config;
+const app = initializeApp(firebaseConfig);
+
+export default app;
